9a49e54bd140623866c11f95a57df92f061c88c8
dimchee <dimchee90@gmail.com>
2019-11-25 19:58:12 +0100

    added map :D

--- Pages/Tabs/mapa.php
+++ Pages/Tabs/mapa.php
@@ -3,28 +3,33 @@
 
 <head>
     <?php include $_SERVER['DOCUMENT_ROOT'].'/zadatakhzs/templates/head.php' ?>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.1.1/css/ol.css" type="text/css">
+    <style>
+    .map {
+        height: 400px;
+        width: 100%;
+    }
+    </style>
+    <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.1.1/build/ol.js"></script>
     <title>Name - Mapa</title>
 </head>
 
 <body>
     <?php include ROOT.'/zadatakhzs/templates/header.php' ?>
-    <div>
-        <?php echo str_replace(".php", "", basename($_SERVER["SCRIPT_NAME"])); ?> 
-    </div>
 
-    <div id="googleMap" style="width:100%;height:400px;"></div>
-
-    <script>
-        function myMap() {
-            var mapProp= {
-                center:new google.maps.LatLng(51.508742,-0.120850),
-                zoom:5,
-            };
-        var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-        }
+    <div id="map" class="map"></div>
+    <script type="text/javascript">
+        var map = new ol.Map({
+            target: 'map',
+            layers: [
+                new ol.layer.Tile({source: new ol.source.OSM()})
+            ],
+            view: new ol.View({
+                center: ol.proj.fromLonLat([37.41, 8.82]),
+                zoom: 4
+            })
+        });
     </script>
-
-    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=myMap"></script>
 
     <?php include ROOT.'/zadatakhzs/templates/footer.php'; ?>
 </body>
