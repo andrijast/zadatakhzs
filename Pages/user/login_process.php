260631bd37b5e033cf517b198b5129e38f7123ab
Andrija Stevanovic <andrija.stevanovic56@gmail.com>
2019-11-25 21:47:40 +0100

    zavrsno

--- Pages/user/login_process.php
+++ Pages/user/login_process.php
@@ -1,5 +1,51 @@
 <?php
 
-$username = POST['username'];
+if (isset($_POST['login-submit'])) {
 
-?>
+    require $_SERVER['DOCUMENT_ROOT'].'/zadatakhzs/templates/connect.php';
+
+    $username = $_POST['user'];
+    $password = $_POST['pass'];
+
+    if (empty($username) || empty($password)) {
+        header("Location: ../user/login_page.php?error_empty");
+        exit();
+    }
+
+    $sql = "SELECT * FROM users WHERE username=?";
+    $stmt = mysqli_stmt_init($connection);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("Location: ../user/login_page.php?error_database");
+        exit();
+    }
+
+    mysqli_stmt_bind_param($stmt, "s", $username);
+    mysqli_stmt_execute($stmt);
+
+    $result = mysqli_stmt_get_result($stmt);
+
+    if ($row = mysqli_fetch_assoc($result)) {
+
+        $pwdCheck = password_verify($password, $row['password']);
+        if (!$pwdCheck) {
+            header("Location: ../user/login_page.php?error_wrongpass");
+            exit();
+        }
+
+        // logged in
+        session_start();
+        $_SESSION['id'] = $row['id'];
+        $_SESSION['user'] = $row['username'];
+
+        header("Location: ../user/login_page.php?login=success");
+        exit();
+    }
+    else {
+        header("Location: ../user/login_page.php?error_nouser");
+        exit();
+    }
+}
+
+else {
+    echo 'Pogresan Ulaz';
+}
