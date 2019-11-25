ef5a66a75dbc84484003ff453030a2eb1500c8cc
Andrija Stevanovic <andrija.stevanovic56@gmail.com>
2019-11-25 11:28:04 +0100

    nesto sitno

--- Templates/header.php
+++ Templates/header.php
@@ -10,10 +10,12 @@
 
 <!-- Menu Bar -->
 <div class="my_menu">
-    <a href="pages/filmoviSajmovi.php"><img src="../res/images/FilmoviSajmovi.jpg" class="my_icon"></a>
-    <a href="test.php"><img src="../res/images/KoncertiIzlozbe.jpg" class="my_icon"></a>
-    <a href="test.php"><img src="../res/images/Mapa.jpg" class="my_icon"></a>
-    <a href="test.php"><img src="../res/images/TakKonkursi.jpg" class="my_icon"></a>
-    <a href="test.php"><img src="../res/images/HumAkcije.jpg" class="my_icon"></a>
-    <a href="test.php"><img src="../res/images/AnketeForumi.jpg" class="my_icon"></a>
+    <ul class="my_menunav">
+        <li><a href="pages/filmoviSajmovi.php"><img src="../res/images/FilmoviSajmovi.jpg" class="my_icon"></a></li>
+        <li><a href="test.php"><img src="../res/images/KoncertiIzlozbe.jpg" class="my_icon"></a></li>
+        <li><a href="test.php"><img src="../res/images/Mapa.jpg" class="my_icon"></a></li>
+        <li><a href="test.php"><img src="../res/images/TakKonkursi.jpg" class="my_icon"></a></li>
+        <li><a href="test.php"><img src="../res/images/HumAkcije.jpg" class="my_icon"></a></li>
+        <li><a href="test.php"><img src="../res/images/AnketeForumi.jpg" class="my_icon"></a></li>
+    </ul>
 </div>
